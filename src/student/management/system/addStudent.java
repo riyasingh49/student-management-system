2bee63c9943e638320aa6d493611fbbfaa1e2017
riyasingh49 <riyasingh454247@gmail.com>
2025-05-01 13:22:29 +0530

    added content

--- src/student/management/system/addStudent.java
+++ src/student/management/system/addStudent.java
@@ -3,6 +3,7 @@ package student.management.system;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -12,6 +13,8 @@ import com.toedter.calendar.JDateChooser;
 public class addStudent extends JFrame {
     JTextField tname, tfname, tcourse, tbranch, tyear, trollno, taddress, tphone, temail;
     JDateChooser tdob;
+    JComboBox branchBox;
+
 
     addStudent(){
         getContentPane().setBackground(new Color(203,215,188));
@@ -23,7 +26,7 @@ public class addStudent extends JFrame {
 
         JLabel name = new JLabel("Name");
         name.setBounds(70, 150,150 , 30);
-        name.setFont(new Font("San serif", Font.BOLD,20));
+        name.setFont(new Font("San serif", Font.BOLD,15));
         add(name);
 
         JTextField tname = new JTextField();
@@ -32,8 +35,8 @@ public class addStudent extends JFrame {
         add(tname);
 
         JLabel fname = new JLabel("Father's Name");
-        fname.setBounds(450, 150,150 , 30);
-        fname.setFont(new Font("San serif", Font.BOLD,20));
+        fname.setBounds(500, 150,150 , 30);
+        fname.setFont(new Font("San serif", Font.BOLD,15));
         add(fname);
 
         JTextField tfname = new JTextField();
@@ -43,13 +46,65 @@ public class addStudent extends JFrame {
 
         JLabel dob = new JLabel("Date of Birth");
         dob.setBounds(70, 200,150 , 30);
-        dob.setFont(new Font("San serif", Font.BOLD,20));
+        dob.setFont(new Font("San serif", Font.BOLD,15));
         add(dob);
 
         tdob = new JDateChooser();
         tdob.setBounds(250, 200, 150, 30);
         tdob.setBackground(new Color(203, 215, 188));
         add(tdob);
+
+        JLabel course = new JLabel("Course");
+        course.setBounds(500, 200, 150, 30);
+        course.setFont(new Font("San serif", Font.BOLD,15));
+        add(course);
+
+        JTextField tcourse = new JTextField();
+        tcourse.setBounds(650, 200, 150, 30);
+        tcourse.setBackground(new Color(203, 215, 188));
+        add(tcourse);
+
+        JLabel branch = new JLabel("Branch");
+        branch.setBounds(70, 250, 150, 30);
+        branch.setFont(new Font("Sans serif",Font.BOLD,15));
+        add(branch);
+
+        
+
+        JTextField tbranch = new JTextField();
+        tbranch.setBounds(250, 250, 150, 30);
+        tbranch.setBackground(new Color(203,215,188));
+        add(tbranch);
+
+        JLabel year = new JLabel("Current year");
+        year.setBounds(500, 250, 150, 30);
+        year.setFont(new Font("Sans serif", Font.BOLD, 15));
+        add(year);
+
+        JTextField tyear = new JTextField();
+        tyear.setBounds(650, 250, 150, 30);
+        tyear.setBackground(new Color(203,215,188));
+        add(tyear);
+
+        JLabel rollno = new JLabel("Roll Number");
+        rollno.setBounds(70, 300, 150, 30);
+        rollno.setFont(new Font("Sans serif", Font.BOLD,15));
+        add(rollno);
+
+        JTextField trollno = new JTextField();
+        trollno.setBounds(250, 300, 150, 30);
+        trollno.setBackground(new Color(203,215,188));
+        add(trollno);
+
+        JLabel address = new JLabel("Address");
+        address.setBounds(500, 300, 150, 30);
+        address.setFont(new Font("Sans serif",Font.BOLD,15));
+        add(address);
+
+        JTextField taddress = new JTextField();
+        taddress.setBounds(650, 300, 150, 30);
+        taddress.setBackground(new Color(203,215,188));
+        add(taddress);
 
         setSize(900, 700);
         setLayout(null);
