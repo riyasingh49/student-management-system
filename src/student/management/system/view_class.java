4f6ad7603c7d220e8ad209d37cf7ecb7b6b89b46
riyasingh49 <riyasingh454247@gmail.com>
2025-05-03 17:47:46 +0530

    system

--- src/student/management/system/view_class.java
+++ src/student/management/system/view_class.java
@@ -5,19 +5,26 @@ import java.sql.ResultSet;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTable;
 
 
 public class view_class extends JFrame{
     Choice sturollno;
+    JTable table;
     view_class(){
 
-        getContentPane().setBackground(new Color(120,117,120));
+        setSize(900,650);
+        setLocation(200, 50);
+        setVisible(true);
+        setLayout(null);
+
+        getContentPane().setBackground(new Color(120,120,130));
         JLabel search = new JLabel("Search by Student Rollno");
-        search.setBounds(20, 0, 150, 30);
+        search.setBounds(20, 10, 170, 30);
         add(search);
 
         sturollno = new Choice();
-        sturollno.setBounds(200, 0, 50, 30);
+        sturollno.setBounds(200, 10, 100, 30);
         add(sturollno);
 
         try {
@@ -28,12 +35,15 @@ public class view_class extends JFrame{
             }
         } catch (Exception e) {
             e.printStackTrace();
-                }
-
-        setSize(900,650);
-        setLocation(200, 50);
-        setVisible(true);
-        setLayout(null);
+        }
+        table = new JTable();
+        try {
+            conn c = new conn();
+            ResultSet resultSet = c.statement.executeQuery("select * from student");
+            table.setModel(DbUtils.resultSetToTableModel(resultSet));
+        } catch (Exception e) {
+            e.printStackTrace();       
+        }
 
     }
     public static void main(String[] args) {
